75c85c269cd01ac1d712516a96fa15243f6f624d
piadascripto <nrkneip@gmail.com>
2023-07-14 02:29:25 +0000

    Holding time adjusts

--- main.py
+++ main.py
@@ -47,18 +47,39 @@ for order in orders:
         long_short = "LONG" if orders_within_trade[symbol][0]["Open/CloseIndicator"] == "O" and orders_within_trade[symbol][0]["Buy/Sell"] == "BUY" else "SHORT"
         trade_result = sum(order["NetCash"] for order in orders_within_trade[symbol])
         trade_amount = sum(abs(order["TradeMoney"]) for order in orders_within_trade[symbol] if order["Open/CloseIndicator"] == "O")
+        trade_shares_quantity = sum(abs(order["Quantity"]) for order in orders_within_trade[symbol] if order["Open/CloseIndicator"] == "O")
         trade_result_percentage = trade_result / trade_amount * 100
         trade_fee = sum(order["IBCommission"] for order in orders_within_trade[symbol])
+        trade_enter_date = min(order["DateTime"] for order in orders_within_trade[symbol])
+        trade_exit_date = max(order["DateTime"] for order in orders_within_trade[symbol])
+        trade_holding_time = trade_exit_date - trade_enter_date
+
+		# Simplify the output based on holding time
+        if trade_holding_time.total_seconds() < 60:  # Less than 60 minutes
+             seconds_apart = trade_holding_time.total_seconds()
+             trade_holding_time_adjusted = str(int(seconds_apart)) + " second" + ("s" if seconds_apart != 1 else "")
+        elif trade_holding_time.total_seconds() < 3600:  # Less than 60 minutes
+             minutes_apart = trade_holding_time.total_seconds() // 60
+             trade_holding_time_adjusted = str(int(minutes_apart)) + " minute" + ("s" if minutes_apart != 1 else "")
+        elif trade_enter_date.date() == trade_exit_date.date():  # Same day
+             hours_apart = trade_holding_time.total_seconds() // 3600
+             trade_holding_time_adjusted = str(int(hours_apart)) + " hour" + ("s" if hours_apart != 1 else "")
+        else:  # Different days
+             days_apart = (trade_exit_date.date() - trade_enter_date.date()).days
+             trade_holding_time_adjusted = str(days_apart) + " day" + ("s" if days_apart != 1 else "")
+		
         trades.append({
             "Symbol": symbol,
             "Long/Short": long_short,
             "Quantity": quantity_sum[symbol],
+			"Trade Shares/Contracts Quantity": trade_shares_quantity,
             "Trade Result": trade_result,
             "Trade Amount": trade_amount,
             "Trade Result Percentage": trade_result_percentage,
             "Trade Fee": trade_fee,
-            "Enter Date": min(order["DateTime"] for order in orders_within_trade[symbol]),
-            "Exit Date": max(order["DateTime"] for order in orders_within_trade[symbol]),
+            "Enter Date": trade_enter_date,
+            "Exit Date": trade_exit_date,
+			"Holding time": trade_holding_time_adjusted,
             "Orders": orders_within_trade[symbol],
         })
         # Reset for next group
